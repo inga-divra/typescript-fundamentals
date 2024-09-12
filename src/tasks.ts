a57070eb520ff89abad1cf4889cd9ddf56a65218
inga-divra <ingady@gmail.com>
2024-09-12 18:46:45 +0300

    Set up task form, input, and task list element references
    Defined Task type and initialized task array;
    Added event listener for form submission, logged task description, and cleared input;
    Added alert for empty input;

--- src/tasks.ts
+++ src/tasks.ts
@@ -1,1 +1,22 @@
-console.log('tasks');
+const taskForm = document.querySelector<HTMLFormElement>('.form')
+const formInput = document.querySelector<HTMLInputElement>('.form-input')
+const taskListElement = document.querySelector<HTMLUListElement>('.list')
+
+type Task = {
+    description: string;
+    isCompleted: boolean
+};
+
+const tasks: Task[] = [];
+
+taskForm?.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const taskDescription = formInput?.value;
+    if (taskDescription) {
+        console.log(taskDescription);
+
+        formInput.value = '';
+        return
+    }
+    alert('Please enter a task description')
+})
